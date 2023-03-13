dec9f93fcfb21973478b9e0fbf8cc3d012c4c55d
emmnedy <115366201+emmnedy@users.noreply.github.com>
2023-03-13 09:34:36 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Visitor!</h1>
+<div>
+  Welcome to Nuel JavaScript resp , kindly follow on Twitter
+  <a href="https://twitter.com/emmnedy0" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
